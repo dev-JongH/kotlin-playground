09e9a15596bd72dd7e4a8a5c320b3a54fc48b167
dev-Jong <dev.whdgus090@gmail.com>
2025-05-30 16:35:31 +0900

    add coroutine test dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
 
     testImplementation(kotlin("test"))
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.2")
 }
 
 tasks.test {
